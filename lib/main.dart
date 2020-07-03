0a3bec9dc0d5623216bb5fc9eb3e8ed8ccb8d8e1
Taras Vovchenko <vtaras15@gmail.com>
2020-07-03 11:44:47 +0300

    init_commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:color_randomizer/randomizer/ColorRandomizer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Color Randomizer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _backgroundColor;
+
+  void _updateBackgroundColor() {
+    setState(() {
+      _backgroundColor = RandomColor();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: _backgroundColor ?? RandomColor(),
+        appBar: AppBar(
+          title: Text("Color Randomizer"),
+        ),
+        body: Container(
+          child: InkWell(
+            onTap: _updateBackgroundColor,
+            child: Center(
+                child: Container(
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.white),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: Center(
+                      child: Text(
+                        "Hey there",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ))),
+          ),
+        ));
+  }
+}
---
+++ lib/randomizer/ColorRandomizer.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+// Creating random color using extension function
+
+// extension RandomColor on Color {
+//   Color generateRandomColor() => Color.fromARGB(_generateNumberForColor(), _generateNumberForColor(),
+//       _generateNumberForColor(), _generateNumberForColor());
+// }
+
+int _generateNumberForColor() => Random().nextInt(257);
+
+class RandomColor extends Color {
+  RandomColor() : super.fromARGB(_generateNumberForColor(),_generateNumberForColor(),_generateNumberForColor(),_generateNumberForColor());
+}
